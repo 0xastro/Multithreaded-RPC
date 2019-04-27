94e560e63ea76557b841946092de7180070ffab3
Astro <m.rahm7n@gmail.com>
2019-04-27 13:42:28 +0200

    Update fifoMutexTEST.c

--- srcV3/fifoMutexTEST.c
+++ srcV3/fifoMutexTEST.c
@@ -26,20 +26,45 @@ unsigned int	init_lock;
  *     Block on the conditional variable: cond_wait     
  */
 
+/* 
+ * Queue_init: Initialize the qlock structures
+ * only once by the first call
+ */
+int Queue_init(){
+	
+	if (!init) {
+		pthread_mutex_init(&(qlock->lock), NULL);
+		pthread_cond_init(&(qlock->->cond), NULL);
+		qlock->->worker = 1U;	/*set the LSB to 1*/
+		qlock->->waiter = 0U;	
+	}
+}
 
-
-pthread_mutex_lock(&qlock->lock) 
-
-	/*Increase the working flag*/
+/* 
+ * Queue_Lock
+ *
+ */
+int Queue_Lock() { 
+	/*Increase the waiter counter*/
 	qlock->waiter = (qlock->waiter)+1;
 	/*Save it to be compared later*/
 	myTicket= qlock->waiter;
 
+	pthread_mutex_lock(&qlock->lock);
 	/*Block If It's not my turn*/
 	while (myTicket != qlock->worker) {
-		pthread_cond_wait(&qlock->cond,&qlock->lcok)
-	}
+		pthread_cond_wait(&qlock->cond,&qlock->lcok);
+	}	
+}
+/* 
+ * Queue_UnLock
+ *
+ */	
+int Queue_UnLock() { 
 	
-	
-pthread_mutex_unlock(&qlock->lock) 
-
+	/*Increase the waiters counter*/
+	qlock->worker = (qlock->worker)+1;
+	pthread_cond_broadcast(&qlock->cond);	
+	pthread_mutex_unlock(&qlock->lock);
+		
+}
